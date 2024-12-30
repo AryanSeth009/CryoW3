3cdc876aa5b922814a878a5568bbb710b8f6b028
Aryanseth009 <setharyan53@gmail.com>
2024-12-30 11:11:28 +0530

    all things are set till pagination of gnews

--- news/app/page.tsx
+++ news/app/page.tsx
@@ -180,6 +180,10 @@ export default function NewsPage() {
   // Define the mobile menu state
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+  // Define loading and error states for each section
+  const [youtubeLoading, setYoutubeLoading] = useState(true);
+  const [youtubeError, setYoutubeError] = useState<string | null>(null);
+
   const fetchGNews = async (query: string = "cryptocurrency") => {
     try {
       setGNewsLoading(true);
@@ -646,6 +650,14 @@ export default function NewsPage() {
       }}
       className="min-h-screen bg-gradient-to-br from-[#0D0B12] text-gray-100 animate-gradient-x"
     >
+      <div className="fixed p-4  inset-0 z-0 select-none pointer-events-none">
+        <div className="absolute top-0 right-0 w-full h-full">
+          {/* Purple gradient lines */}
+          <div className="absolute top-0 right-0 w-1/2 h-full bg-[linear-gradient(45deg,transparent_45%,rgba(123,97,255,0.1)_45%,rgba(123,97,255,0.1)_55%,transparent_55%)]" />
+          <div className="absolute top-0 right-0 w-1/2 h-full bg-[linear-gradient(45deg,transparent_35%,rgba(123,97,255,0.1)_35%,rgba(123,97,255,0.1)_45%,transparent_45%)]" />
+          <div className="absolute top-0 right-0 w-1/2 h-full bg-[linear-gradient(45deg,transparent_25%,rgba(123,97,255,0.1)_25%,rgba(123,97,255,0.1)_35%,transparent_35%)]" />
+        </div>
+      </div>
       <nav className="sticky top-0 z-50 bg-gray-900/90 backdrop-blur-xl border-b border-gray-800 shadow-lg p-4">
         <div className="container mx-auto flex items-center justify-between">
           <h1 className="text-2xl font-bold text-purple-500">CryoW3Times</h1>
@@ -855,6 +867,142 @@ export default function NewsPage() {
                           </div>
                         ))}
                       </div>
+                    </section>
+
+                    {/* Gnews Section*/}
+                    <section className="mb-12 flex flex-col">
+                      <div className="flex mb-4">
+                        <h2 className="text-4xl w-full font-normal font-sans flex items-center justify-between">
+                          GNews Cryptocurrency
+                        </h2>
+                        <div className="flex gap-2 mb-4 items-end justify-end">
+                          <Button
+                            variant="outline"
+                            size="icon"
+                            disabled={gNewsCurrentPage === 1}
+                            onClick={() =>
+                              handleGNewsPageChange(gNewsCurrentPage - 1)
+                            }
+                            className="h-8 w-8 rounded-full border-gray-800"
+                          >
+                            <ArrowRightIcon className="rotate-180" />
+                          </Button>
+                          <Button
+                            variant="outline"
+                            size="icon"
+                            disabled={
+                              gNewsCurrentPage * gNewsItemsPerPage >=
+                              gNews.length
+                            }
+                            onClick={() =>
+                              handleGNewsPageChange(gNewsCurrentPage + 1)
+                            }
+                            className="h-8 w-8 rounded-full border-gray-800"
+                          >
+                            <ArrowRightIcon />
+                          </Button>
+                        </div>
+                      </div>
+                      {gNewsLoading ? (
+                        <div className="flex justify-center items-center">
+                          <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-purple-500"></div>
+                        </div>
+                      ) : gNewsError ? (
+                        <div className="text-red-500 text-center">
+                          {gNewsError}
+                        </div>
+                      ) : (
+                        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-3">
+                          {gNews
+                            .slice(
+                              (gNewsCurrentPage - 1) * gNewsItemsPerPage,
+                              gNewsCurrentPage * gNewsItemsPerPage
+                            )
+                            .map((article, index) => (
+                              <Link
+                                key={index}
+                                href={article.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group"
+                              >
+                                <Card className="bg-[#0A0B0F]/60 backdrop-blur-xl border-white/10 rounded-xl hover:rounded-2xl hover:border-[#8B5CF6]/50 hover:border-purple-500/50 transition-all duration-300 hover:shadow-xl hover:shadow-purple-500/10 transform hover:-translate-y-1">
+                                  <div className="relative h-48 rounded-t-xl overflow-hidden">
+                                    <Image
+                                      src={
+                                        article.urlToImage ||
+                                        DEFAULT_FALLBACK_IMAGE
+                                      }
+                                      alt={article.title}
+                                      fill
+                                      className="object-cover transition-transform duration-700 group-hover:scale-105"
+                                    />
+                                    <div className="absolute inset-0 bg-gradient-to-t from-[#231d30] via-transparent opacity-50" />
+                                    <Badge className="absolute top-4 left-4 bg-purple-500/90 hover:bg-purple-600">
+                                      {article.source.name}
+                                    </Badge>
+                                  </div>
+                                  <div className="p-4">
+                                    <h3 className="font-semibold text-md leading-tight group-hover:text-purple-500 transition-colors line-clamp-2 mb-3">
+                                      {article.title}
+                                    </h3>
+                                    <p className="text-sm text-gray-400 line-clamp-3 mb-4">
+                                      {article.description}
+                                    </p>
+                                    <div className="flex items-center gap-4 text-sm text-gray-400">
+                                      <div className="flex items-center gap-2">
+                                        <Avatar className="h-6 w-6 border-2 border-purple-500/30">
+                                          <Image
+                                            src={
+                                              article.source.icon ||
+                                              DEFAULT_FALLBACK_IMAGE
+                                            }
+                                            alt="Source"
+                                            width={24}
+                                            height={24}
+                                          />
+                                        </Avatar>
+                                        <span className="font-small">
+                                          {article.source.name}
+                                        </span>
+                                      </div>
+                                      <span>•</span>
+                                      <div className="flex items-center gap-2 opacity-60">
+                                        <span>
+                                          {new Date(
+                                            article.publishedAt
+                                          ).toLocaleDateString()}
+                                        </span>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </Card>
+                              </Link>
+                            ))}
+                        </div>
+                      )}
+                      {/* <div className="flex justify-center gap-4 mt-6">
+                        <button
+                          onClick={() =>
+                            handleGNewsPageChange(gNewsCurrentPage - 1)
+                          }
+                          disabled={gNewsCurrentPage === 1}
+                          className="px-4 py-2 bg-purple-500 text-white rounded-lg disabled:bg-gray-400"
+                        >
+                          Previous
+                        </button>
+                        <button
+                          onClick={() =>
+                            handleGNewsPageChange(gNewsCurrentPage + 1)
+                          }
+                          disabled={
+                            gNewsCurrentPage * gNewsItemsPerPage >= gNews.length
+                          }
+                          className="px-4 py-2 bg-purple-500 text-white rounded-lg disabled:bg-gray-400"
+                        >
+                          Next
+                        </button>
+                      </div> */}
                     </section>
 
                     {/* Reddit Section */}
